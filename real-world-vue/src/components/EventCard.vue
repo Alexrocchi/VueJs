6af234b4fb39600de55c333216823c7018cb1fad
Alexrocchi <alexrocchi.a@gmail.com>
2023-05-01 17:54:35 +0200

    Use sass

--- real-world-vue/src/components/EventCard.vue
+++ real-world-vue/src/components/EventCard.vue
@@ -14,16 +14,16 @@ defineProps({
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
   .event-card {
     padding: 20px;
     width: 250px;
     cursor: pointer;
     border: 1px solid #39495c;
     margin-bottom: 18px;
-  }
-  .event-card:hover {
-    transform: scale(1.01);
-    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+    &:hover {
+      transform: scale(1.01);
+      box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+    }
   }
 </style>
